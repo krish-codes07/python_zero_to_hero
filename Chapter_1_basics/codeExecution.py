f83a8aa115688db9eecdec43c124375c2741fc2d
krish-codes07 <krishshrivastava124@gmail.com>
2026-01-25 12:38:24 +0530

    initial commit chapter 0 setup and chapter 1 basics

---
+++ Chapter_1_basics/codeExecution.py
@@ -0,0 +1,11 @@
+# name = "Krish Kumar"
+# age = 18
+# x = 10
+# y = 20
+# print("Actual Value :",x)
+# x=y
+# print("Changed value :",y)
+print("Hello world")
+print("python is strict about syntax errors")
+print(10)
+print(10+20)
---
+++ Chapter_1_basics/indentation_demo.py
@@ -0,0 +1,8 @@
+if True: 
+    print("this is inside the if") #here indentation matter more because python does not braces like c instead it uses indentation(spaces)
+    print("still inside if")
+
+print("this is outside if")
+if False:
+    print("Hi")
+print("Bye")
