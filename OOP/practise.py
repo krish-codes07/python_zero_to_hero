e3a9b512b06a268db6c2f47fa5c745e58355fa30
krish-codes07 <krishshrivastava124@gmail.com>
2026-03-18 22:10:39 +0530

    basic library system using class and objects

--- OOP/practise.py
+++ OOP/practise.py
@@ -46,37 +46,91 @@
 #     w.show()
 
 # problem 3: student report card show student info , average marks, total marks, grade 
-class Student:
-    def __init__(self,name, roll_no , *marks):
-        self.name = name
-        self.roll_no = roll_no
-        self.marks = marks
-    def get_total(self):
-        return sum(self.marks)
-    def get_average(self):
-        return round(self.get_total()/5,2)
-    def get_grade(self):
-        average = self.get_average()
-        if(average >= 90):
-            return "A"
-        elif(average >= 75 and average < 90):
-            return "B"
-        elif(average >=60 and average < 75):
-            return "C"
-        elif(average < 60):
-            return "F"
-    def show_report(self):
-        print("-----Report Card-----")
-        print(f"Name : {self.name}")
-        print(f"Roll No : {self.roll_no}")
-        print(f"Total : {self.get_total()}")
-        print(f"Average : {self.get_average()}")
-        print(f"Grade : {self.get_grade()}")
-name = input("Student name : ")
-roll_no = int(input("student roll no : "))
-marks = []
-for i in range(5):
-    m = int(input(f"enter marks of {i+1} subject : "))
-    marks.append(m)
-s = Student(name , roll_no , *marks)
-s.show_report()
+# class Student:
+#     def __init__(self,name, roll_no , *marks):
+#         self.name = name
+#         self.roll_no = roll_no
+#         self.marks = marks
+#     def get_total(self):
+#         return sum(self.marks)
+#     def get_average(self):
+#         return round(self.get_total()/5,2)
+#     def get_grade(self):
+#         average = self.get_average()
+#         if(average >= 90):
+#             return "A"
+#         elif(average >= 75 and average < 90):
+#             return "B"
+#         elif(average >=60 and average < 75):
+#             return "C"
+#         elif(average < 60):
+#             return "F"
+#     def show_report(self):
+#         print("-----Report Card-----")
+#         print(f"Name : {self.name}")
+#         print(f"Roll No : {self.roll_no}")
+#         print(f"Total : {self.get_total()}")
+#         print(f"Average : {self.get_average()}")
+#         print(f"Grade : {self.get_grade()}")
+# name = input("Student name : ")
+# roll_no = int(input("student roll no : "))
+# marks = []
+# for i in range(5):
+#     m = int(input(f"enter marks of {i+1} subject : "))
+#     marks.append(m)
+# s = Student(name , roll_no , *marks)
+# s.show_report()
+
+# problem 4 : library book system which has methods issue book , Return book , book info 
+class Book:
+    def __init__(self,total_copies , title, author , issued_copies = 0):
+        self.title = title
+        self.author = author 
+        self.total_copies = total_copies
+        self.issued_copies = issued_copies
+
+    def issue_book(self):
+        if(self.total_copies > 0):
+            self.total_copies -= 1
+            self.issued_copies += 1
+            print(f"Book Issued! Remaining copies {self.total_copies}")
+        elif(self.total_copies == 0):
+            print("No copies available for this book.")
+        else:
+            print("Please choose another book.")
+
+    def return_book(self):
+        if(self.issued_copies > 0):
+            print("Book Returned.")
+            self.total_copies += 1
+            self.issued_copies -= 1
+            print(f"Total copies = {self.total_copies}")
+        elif(self.issued_copies == 0 ):
+            print("Nothing to return. ")
+
+    def available_copies(self):
+        if (self.total_copies > 0):
+            return (self.total_copies - self.issued_copies)
+        
+    def show_info(self):
+        print("-----Book Info-----\n\n")
+        print(f"Title : {self.title}")
+        print(f"Author : {self.author}")
+        print(f"Total Copies : {self.total_copies}")
+        print(f"issued : {self.issued_copies}")
+        print(f"Avaiable copies : {self.available_copies()}")
+
+title = input("Enter Book name : ")
+author = input("Author Name : ")
+total_copies = int(input("Copies : "))
+l = Book(total_copies , title , author)
+while(True):
+    a = int(input("What do you want to do?\n1.Issue Book\n2.Return book\n3.Show Info\n4.Exit\n"))
+    if(a == 1):
+        l.issue_book()
+    elif(a == 2):
+        l.return_book()
+    elif(a == 3):
+        l.show_info()
+    elif(a == 4):
+        break
